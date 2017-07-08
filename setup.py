4d70621f462bdddc828d2298fccfb466aa9dc82d
r.rader <r.rader@smartweb.com.ua>
2017-07-08 11:31:11 +0300

    setup.y

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='zk_phone',
+    version='1.0',
+    description='Zakupki Phone',
+    author='Roman Rader',
+    author_email='antigluk@gmail.com',
+    url='https://github.com/rrader/zk_phone',
+    install_requires=['pad4pi==1.0.0', 'charlcd==0.4.0', 'RPi.GPIO==0.6.3'],
+    entry_points={
+        'console_scripts': [
+            'zk_phone = zk_phone.main:main',
+        ],
+    },
+)
