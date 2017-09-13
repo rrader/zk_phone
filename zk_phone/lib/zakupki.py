239fb73a1d318e09c335d660f9c2f2665f7476f2
r.rader <r.rader@smartweb.com.ua>
2017-09-13 14:02:46 +0300

    fix version

--- zk_phone/lib/zakupki.py
+++ zk_phone/lib/zakupki.py
@@ -5,6 +5,6 @@ import requests
 def zk_version():
     try:
         data = requests.get('https://ai.uaprom/stats/zkprom', verify=False).json()
-        return data['last_migration']['source_title']
+        return data['by_version']['zkprom1']['cfg']['title']
     except:
         return None
